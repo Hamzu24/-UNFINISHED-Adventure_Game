780cb492445fb8d47538dd4b5b18415235b705d4
Hamza Pereira <64593299+Hamzu24@users.noreply.github.com>
2022-09-19 14:48:47 +0100

    Initial Commit

---
+++ Choose Your Adventure HW1.py
@@ -0,0 +1,82 @@
+doorways = ["You find yourself outisde your house, on a stormy night. Do you want to move through the doorway or climb on the roof (For all the choices either type 1 or 2)?",
+                ["You go through the doorway and find a wild boar charging towards you. Do you retreat outside, or try to run past him?",
+                     ["You run outside, but quickly the boar bashes through the doorway. The door flies forward, and whacks you in the face, knocking you down. Do you crawl backwards and stand up or fight?",
+                         ["You try to turn your body backwards, but before you can move, you get gored by the boar. You Die!"],
+                         ["You stand up, and suddenly feel confident in your fighting skills. You charge at the boar. It Stops, and suddenly looks confused. You whack it in the face, and it scurries away, shocked about the outburst. Do you follow it or enter the house?",
+                             ["You charge after the boar, but before you know it, you seem to be lost in the woods. Do you decide to try to find your way back, or survive the wilderness?",
+                                 ["You run through the forest, not quite sure of where you're going, and stumble across the same boar, sleeping in a patch of grass. Do you fight the boar, or ignore it?",
+                                     ["You attack the boar with a sharpened stick, and spear it without thinking twice. It quickly dies. Disturbed by your bloodlust you stand still, and only realise too late that a whole family of boars surround you. They quickly close in. You Die!"],
+                                     ["You quietly tiptoe past it, and ask yourself why you ever even considered attacking it. After a couple miles of walking, you see the house. Do you enter the house or climb the roof?"]
+                                 ],
+                                 ["You attempt to establish a life in the forest, by surviving on berries and animal carcasses. After several years, the solitary life that you lead starts to become unbearable, and you slowly realise that you are insane. The rest of your life is a blur, and at the age of 56 you get gored by a boar. You Die!"]
+                             ],
+                             ["You enter your house, but soon realise that it's changed. The house is made purely out of old, rotting wood, and the only clear pathway is leading upstairs, because the whole bottom floor has been rotted away, revealing a large hole. Do you descend the hole or climb the stairs?",
+                                 ["Only realising too late that you have no equipment, you tumble down the pit, smashing into countless rocks, but eventually land in a pool. Do you swim down, or rise to the surface?",
+                                     ["You swim down to the bottom, and see some diving equipment. You quikckly put it on, and are now able to see much better. You are in a large metal pool. The floor of the pool is blanketed with huge fish carcasses and skeletons. You see a sign ahead of you, but a platform behind you. Do you swim towards the sign or the platform?",
+                                         ["You swim towards the sign, and read the words: 'WARNING: MEGALODON'. Remembering that sharks can smell blood, you feel like you're in grave danger. However, you also notice what seems to be an underwater cave entrance around 50 metres ahead of you. Do you swim towards the platform or the cave?"],
+                                         ["You clamber onto the platform, and start walking for around 100 metres. You come across a large metal door, seemingly reinforced, with a small security house next to it. Do you sneak up to the security house, or run screaming?"]
+                                     ],
+                                     ["You barely clamber onto a platform, profusely bleeding from your injuries. The gravity of your situation suddenly becomes apparent, and you faint, falling into the pool. You Drown!"]
+                                 ],
+                                 ["You climb up the stairs, and come across a junction, with a seemingly bloodstained door with a skull on the left, and a rotten door on the right. Which path do you take?"]
+                             ]
+                         ]
+                     ],
+                     ["You attempt to run past the wild boar, but he headbutts you into the wall. Your last moments are spent in pain. You Die!",
+                     ]
+                 ],
+                ["You climb the roof and see a chimney, and an air vent. Which one do you enter?",
+                     ["You attempt to slide down the chimney, but get stuck halfway through. You suddenly feel extremely claustrophobic. Do you attempt to slide more or try to rise up?",
+                        ["As you slide down the chimney, you realise that it's started raining extremely hard. As you slide dow the last for meters, to your horror you realise that it's a fake chimney, and has a sealed bottom. You slowly drown..."],
+                        ["You rise up, barely making it out."]
+                     ],
+                     ["You hear the wild grunts of a boar exiting the house. You feel like you narrowly escaped a tragic death... You reach a junction in the air vent. Do you take a left turn, or a right turn?",
+                         ["You turn left, and come across a large fan blocking the path ahead, with an open wire box next to it. You can either attempt to break the wires in the box, or turn around and go the other path.",
+                             ["You try to pull the wires in the box, and successfully rip them out. However, the fan ahead of you appears unchanged, and you can hear the floor beneth you slowly sliding away. Before you can react, you topple down onto a wooden floor. You cautiosuly rise, and can see that you're at the end of a long hallway, with a door behind you. Do you go down the hallway, or go in a door behind you?",
+                                 ["You walk down the hallway. After several hours of walking, you are yet to see an end of the hallway. However, you can see some pills on the floor before you. Do you carry on walking, or take the pills?"],
+                                 ["You go in the door behind you, and see several armed guards sitting around a table. They appear to be sitting still, positioned in a way so you can't see their face. Do you go towards them, or observe them for a while longer?",
+                                     ["You walk towards them, and realise that they are long dead, with faces of rotting flesh, unnaturally cut. Do you take their guns, or ignore them?",
+                                         ["You try to take their guns, but to your surprise they turn around in unison, and shoot several round into your feet. You slowly bleed to death on the floor. You Die!"],
+                                         ["You ignore them, and look around the room. Before you can draw any conclusions, one of them turns around and shoots a round into your back. Your fall forwards, and your face lands on a sharpened metal spike, that skewers your brain. You Die!"]
+                                     ],
+                                     ["You observe them for a while, and notice dried bloodstains on the floor and walls around them. You slowly come to the conclusion that they are dead. Do you take their guns, or ignore them?",
+                                         ["You attempt to steal their guns, but to your surprise they turn around, exposing grotesquely rotted features, and shoot several rounds into your head and chest. You Die!"],
+                                         ["You ignore them, and look around the room. There is an air vent entrance to your right, and a trapdoor beneath your feet. Which one do you go down?",
+                                             ["You choose the air vent. After crawling a couple meters, you hear the clanging of several men entering the air vent behind you, and to your horror the dead soldiers are reaching out for you, trying to drag you out of the vent. Do you fight them, or crawl faster?",
+                                                 ["You attempt to kick their faces, but one grabs your foot, and pulls you out. Before you can block, he claws your stomach, and rips out your stomach. The other grabs your head and perpetually smashes it against the ground. You Die!"],
+                                                 ["You crawl faster, and the dead men begin to crawl after you. After several minutes, your body parts are bggining to ache, and the soldiers are only about half a metre behind you. However, slavation is ahead. You can see the end of the vent. Do you crawl faster or maintain this pace?",
+                                                     ["You attempt ot crawl even faster, but end up tearing a ligament, and lay on the vent floor, bleeding out. The soldiers continously bang your legs against the top and bottom of the vent, and unable to move, you die a slow death..."],
+                                                     ["You wisely maintain a steady pace, and just make it out, shutting a grill behind you to block the soldiers. The room you are in is a dazzling white, and is brightly lit. Before you are three chests, one with a red padlock, and one with a blue padlock (the keys are beside the locks). Which do you open?",
+                                                         ["You open the chest with a red padlock, and see a strip of paper, announcing you the victor of the story, finding one of the possible five victories that you could have gotten. Suddenly, all the hardships you've encountered along the way have seem worth it! You Win!"],
+                                                         ["You open the chest with a blue padlock, and see a piece of rotting flesh inside. You try to look away, but find that your eyes are glued to the object. It appears to slowly bulge, and expand in your gaze, until all you can see is the rotting corpse of a soldier, lumbering towards you, grasping your face, and tearing your eyes out. You Die!"]
+                                                     ]
+                                                 ]
+                                             ],
+                                             ["You fall down the trapdoor, and land in what seems to be a large lake, without any lighting. As you walk on, you hear two people jump into the water behind you, and slide down into the water. However, when you turn around you see nothing. Do you stand still, or run towards a wall?"]
+                                         ]
+                                     ]
+                                 ]
+                             ],
+                             ["You turn around, but only realise that the space you are in is too small to turn around in. You end up positiong yourself in an unnatural curl, and snapping your neck. You Die!"]
+                         ],
+                         ["You turn right, and the lights in the tunnel slowly get dimmer and dimmer until the path ahead of you is pitch black. You have a bad feeling about the path ahead, so can either turn around or continue on.",
+                             ["You decide to double back on yourself, and start crawling backwards, due to a lack of space to reorient yourself. After a couple metres, the lights ahead of you flicker on, and reveal a large metal door. This door quickly slides open, allowing a huge boar to charge at you. Do you move backwards or forwards?",
+                                 ["You slowly edge backwards, seeing the boar quickly advancing towards you. Just as it's about to hit you, you see a loose vent panel ahead of you. However, the boar blocks the path, and your death is inevitable. The boar penetrates your brain with a tusk, and smashes you against both sides of the vent several times, reducing you to a pulp. You Die!"],
+                                 ["You move forward, even though it goes against all your survival instincts, and luckily see a loose vent panel on your right. You punch it several times, and fall into it. You fall onto a concrete floor, and get knocked out by the impact. When you wake up, you see that You're in a large concrete ring, with several skeletons laid out around the circle. As you try to move, you realise that your arms and legs are tied to the floor. Do you struggle, or scream for help?",
+                                     [],
+                                     []
+                                 ]
+                             ],
+                             ["You continue on, but hit what seems to be a door. This door opens before you, and something spears your face, piercing your eye. Before you have a chance to scream, you get rammed against the wall of the vent, and your insides explode. You Die!"]
+                         ]
+                     ]
+                 ]
+           ]
+dead = 0
+dead = False
+currentChoice = doorways
+choice = 0
+
+while dead != True:
+    choice = int(input(currentChoice[0]))
+    currentChoice = currentChoice[choice]
